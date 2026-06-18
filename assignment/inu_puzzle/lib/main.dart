4f0be458e3ceec126f6ce8c9d2041f320c7427cf
Rohit Tanwar <rohittanwar1287@gmail.com>
2026-06-18 11:18:30 +0530

    app logo changed

--- assignment/inu_puzzle/lib/main.dart
+++ assignment/inu_puzzle/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
-      title: 'INU Puzzle',
+      title: 'Pawzle',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: const HomeScreen(),
     );
