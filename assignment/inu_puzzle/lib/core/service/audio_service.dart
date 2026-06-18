e58d5d584cd89a28aca08d45fc5ad671421700d6
Rohit Tanwar <rohittanwar1287@gmail.com>
2026-06-18 12:29:52 +0530

    sound fix

--- assignment/inu_puzzle/lib/core/service/audio_service.dart
+++ assignment/inu_puzzle/lib/core/service/audio_service.dart
@@ -1,55 +1,48 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
 
 class AudioService extends GetxService {
   final AudioPlayer _player = AudioPlayer();
 
+  final isSoundEnabled = true.obs;
+
   Future<void> win() async {
+    if (!isSoundEnabled.value) return;
+
     await _player.setReleaseMode(ReleaseMode.release);
-    return _player.play(AssetSource('audio/youwon.mp3'));
+    await _player.play(AssetSource('audio/youwon.mp3'));
   }
 
   Future<void> lose() async {
+    if (!isSoundEnabled.value) return;
+
     await _player.setReleaseMode(ReleaseMode.release);
-    return _player.play(AssetSource('audio/gameover.mp3'));
+    await _player.play(AssetSource('audio/gameover.mp3'));
   }
 
   Future<void> timeisabouttoUp() async {
+    if (!isSoundEnabled.value) return;
+
     await _player.setReleaseMode(ReleaseMode.release);
-    return _player.play(AssetSource('audio/timeisabouttoup.mp3'));
+    await _player.play(AssetSource('audio/timeisabouttoup.mp3'));
   }
 
   Future<void> backgroundMusic() async {
+    if (!isSoundEnabled.value) return;
+
     await _player.setReleaseMode(ReleaseMode.loop);
-
     await _player.play(AssetSource('audio/backgroundmusic.mp3'), volume: 0.5);
-
-    isPlaying.value = true; // <-- ADD THIS
   }
 
-  Future<void> stopBackgroundMusic() async {
-    await _player.stop();
-  }
+  Future<void> toggleSound() async {
+    isSoundEnabled.toggle();
 
-  final isPlaying = false.obs;
-
-  @override
-  void onInit() {
-    super.onInit();
-
-    _player.onPlayerStateChanged.listen((state) {
-      isPlaying.value = state == PlayerState.playing;
-    });
-  }
-
-  bool isBackgroundMusicPlaying() => isPlaying.value;
-
-  Future<void> toggleBackgroundMusic() async {
-    if (_player.state == PlayerState.playing) {
-      await _player.pause();
+    if (!isSoundEnabled.value) {
+      // Mute everything
+      await _player.stop();
     } else {
-      await _player.resume();
+      // Start background music again
+      await backgroundMusic();
     }
   }
 }
